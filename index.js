a019380282e563a727b5e130b1d2310b1a71bf74
vxrshxx <124232595+vxrshxx@users.noreply.github.com>
2023-03-26 12:42:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import App from './App';
+
+import ReactDOM from 'react-dom';
+
+
+
+
+
+ReactDOM.render(
+<React.StrictMode>
+	<App />
+</React.StrictMode>,
+document.getElementById('root')
+);
+
